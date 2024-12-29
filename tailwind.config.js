33728f8ffa5f3a63ef901b7ed6770796cddc6e55
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2024-12-29 08:52:34 +0700

    dawg tailwind is working now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}', // Semua file di src
-  ],
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
